0a723099e566b43f2f114f25aa8631865a15ff77
fankouzu <1046166@qq.com>
2021-03-18 20:11:35 +0800

    Updates

---
+++ precache-manifest.f3401356af0031c13e3b661a88f554cf.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0116f03b949f5a5250c9057b4ceb3cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f5484771aa015e853f2",
+    "url": "/static/css/main.cf602f5d.chunk.css"
+  },
+  {
+    "revision": "3d900161b5330c82f3d4",
+    "url": "/static/js/0.0e21f240.chunk.js"
+  },
+  {
+    "revision": "711b82d6ff28fa499465",
+    "url": "/static/js/10.ffa34677.chunk.js"
+  },
+  {
+    "revision": "77173c59c201e90abcbd",
+    "url": "/static/js/3.631559a6.chunk.js"
+  },
+  {
+    "revision": "2ea0bd56bca17deb6aeb434005a4f09a",
+    "url": "/static/js/3.631559a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1851d027f22425694fdf",
+    "url": "/static/js/4.5c065d9c.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/static/js/4.5c065d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831bf9a8cb0444c85d9c",
+    "url": "/static/js/5.2ba338cf.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/5.2ba338cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113210e4652ff230663e",
+    "url": "/static/js/6.2c4330a9.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/6.2c4330a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "477f86d095e3e269c1bc",
+    "url": "/static/js/7.3cee4d04.chunk.js"
+  },
+  {
+    "revision": "7565f864660a15fdd362",
+    "url": "/static/js/8.498d40b7.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.498d40b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35493b199b9d7d0de51",
+    "url": "/static/js/9.b500f4ed.chunk.js"
+  },
+  {
+    "revision": "8f5484771aa015e853f2",
+    "url": "/static/js/main.d4edce2d.chunk.js"
+  },
+  {
+    "revision": "19d3746ffc0ffdc3ad2d",
+    "url": "/static/js/runtime-main.6ef99385.js"
+  },
+  {
+    "revision": "15104bece2e253e99445b69a639620fb",
+    "url": "/static/media/BETH.15104bec.svg"
+  },
+  {
+    "revision": "ee5617c39c1d29a9dd6ddfaaae49d155",
+    "url": "/static/media/LINK.ee5617c3.svg"
+  },
+  {
+    "revision": "8e72f6f2edbc451e3bba41e50d2b8fcd",
+    "url": "/static/media/MDX.8e72f6f2.png"
+  },
+  {
+    "revision": "e2b742d6f5fd75d573863c11fce06fd7",
+    "url": "/static/media/UNI.e2b742d6.svg"
+  },
+  {
+    "revision": "839a0f0d6e0a69fed8d7846344d2100b",
+    "url": "/static/media/USDT.839a0f0d.png"
+  },
+  {
+    "revision": "b2b62f02e6053e8e3daf2b95df67e08b",
+    "url": "/static/media/YFI.b2b62f02.svg"
+  },
+  {
+    "revision": "4753d28db188d103d79e33073e85b113",
+    "url": "/static/media/background_2.4753d28d.jpg"
+  },
+  {
+    "revision": "d612bee1e167df4e2f85ebe948743e6b",
+    "url": "/static/media/background_3.d612bee1.jpg"
+  },
+  {
+    "revision": "68ae2e94bcc33b092d0e912ab40e4f88",
+    "url": "/static/media/farmer.68ae2e94.png"
+  },
+  {
+    "revision": "211fe839dccc8742afe2d5ac49a77a26",
+    "url": "/static/media/gocash.211fe839.png"
+  },
+  {
+    "revision": "160112f78ee29aa5edfc542db02e21ed",
+    "url": "/static/media/goswap-GOB.160112f7.svg"
+  },
+  {
+    "revision": "73568d603e1f6047fdd47609d75d39d7",
+    "url": "/static/media/goswap-GOC.73568d60.svg"
+  },
+  {
+    "revision": "48ba4fe80133ade79afeea9ac7d9d31a",
+    "url": "/static/media/goswap-GOS.48ba4fe8.svg"
+  },
+  {
+    "revision": "1d38fc66aef967257a27c3be0ea76b01",
+    "url": "/static/media/goswap-GOT.1d38fc66.svg"
+  }
+]);
